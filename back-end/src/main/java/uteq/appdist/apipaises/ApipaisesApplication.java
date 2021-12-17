f40ad0b68018a20460647d66052714d8daabeafb
Jose Mario Solorzano Cabrera <jose.solorzano2016@uteq.edu.ec>
2021-12-16 21:57:06 -0500

    Projects Scaffolding
    The projects and base structure of the repository were instantiated

---
+++ back-end/src/main/java/uteq/appdist/apipaises/ApipaisesApplication.java
@@ -0,0 +1,12 @@
+package uteq.appdist.apipaises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipaisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipaisesApplication.class, args);
+	}
+}
---
+++ back-end/src/test/java/uteq/appdist/apipaises/ApipaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package uteq.appdist.apipaises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
